336c9707fb75c8ea2c6dcf8258351a15a0e871f8
Rogerio Fonseca Gonçalves <rogerio.fonseca@gmail.com>
2019-10-20 21:01:54 -0300

    List edited

--- src/main/java/br/com/rogerius/examples/Customer.java
+++ src/main/java/br/com/rogerius/examples/Customer.java
@@ -1,22 +1,32 @@
 package br.com.rogerius.examples;
 
-public class Customer {
+import org.apache.commons.lang3.builder.DiffBuilder;
+import org.apache.commons.lang3.builder.DiffResult;
+import org.apache.commons.lang3.builder.Diffable;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
-	private Integer id;
+public class Customer implements Diffable<Customer> {
+
+	private String id;
 	private String name;
 	private Address address;
 
-	public Customer(int id, String name, String locality, String city) {
+	public Customer(String id, String name, String locality, String city) {
 		this.setId(id);
 		this.setName(name);
 		this.setAddress(new Address(locality,city));
 	}
 
-	public Integer getId() {
+	public Customer(String id, String name) {
+		this.setId(id);
+		this.setName(name);
+	}
+
+	public String getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 
@@ -34,5 +44,48 @@ public class Customer {
 
 	public void setAddress(Address address) {
 		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [name=" + name + "]";
+	}
+
+	@Override
+	public DiffResult diff(Customer obj) {
+		return new DiffBuilder(this, obj, ToStringStyle.SHORT_PREFIX_STYLE)
+				.append("name", this.getName(), obj.getName())
+				.build();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 }
--- src/test/java/br/com/rogerius/sets_examples/SetsTest.java
+++ src/test/java/br/com/rogerius/sets_examples/SetsTest.java
@@ -5,10 +5,11 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.LinkedList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.builder.DiffResult;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,34 +17,73 @@ import br.com.rogerius.examples.Customer;
 
 public class SetsTest {
 
-	List<Customer> list1 = new ArrayList<Customer>();
-	List<Customer> list2 = new ArrayList<Customer>();
+	List<Customer> version1 = new ArrayList<Customer>();
+	List<Customer> version2 = new ArrayList<Customer>();
 	List<Customer> list3 = new ArrayList<Customer>();
 
 	@Before
 	public void setUp() {
-		Customer customer1 = new Customer(1, "Daniel", "locality1", "city1");
-		Customer customer2 = new Customer(2, "Fredrik", "locality2", "city2");
-		Customer customer3 = new Customer(3, "Kyle", "locality3", "city3");
-		Customer customer4 = new Customer(4, "Bob", "locality4", "city4");
-		Customer customer5 = new Customer(5, "Cat", "locality5", "city5");
-		Customer customer6 = new Customer(6, "John", "locality6", "city6");
+		Customer a1 = new Customer("1", "A1");
+		Customer b1 = new Customer("2", "B1");
+		Customer c1 = new Customer("3", "C1");
+		
+		Customer a2 = new Customer("1", "A1");
+		Customer b2 = new Customer("2", "B2");
+		Customer d1 = new Customer("4", "D1");
+		Customer e1 = new Customer("5", "E1");
 
-		list1 = Arrays.asList(customer1, customer2, customer3);
-		list2 = Arrays.asList(customer4, customer5, customer6);
-		list3 = Arrays.asList(customer1, customer2);
-
-		List<Customer> linkedList1 = new LinkedList<Customer>(list1);
+		version1 = Arrays.asList(a1, b1, c1);
+		version2 = Arrays.asList(a2, b2, d1,e1);
+		
 	}
 
-	@Test
+//	@Test
 	public void givenCustomerListAndASubcollection_whenChecked_thenTrue() {
-		assertTrue(CollectionUtils.isSubCollection(list3, list1));
+		assertTrue(CollectionUtils.isSubCollection(version1, version2));
+	}
+
+	List<DiffResult> diferencas = new ArrayList<DiffResult>();
+	@Test
+	public void givenTwoLists_whenIntersected_thenCheckSize() {
+		Collection<Customer> intersection = CollectionUtils.intersection(version1, version2);
+//		System.out.println("Intersection");
+//		CollectionUtils.
+//		intersection.forEach(i -> System.out.println(i.getName()));
+		
+//		for (Customer customer : intersection) {
+//			diferencas.add();
+//		}
+//		assertTrue(intersection.size() == 2);
 	}
 	
 	@Test
-	public void givenTwoLists_whenIntersected_thenCheckSize() {
-	    Collection<Customer> intersection = CollectionUtils.intersection(list1, list3);
-	    assertTrue(intersection.size() == 2);
+	public void givenTwoLists_whenSubtracted_thenCheckElementNotPresentInA() {
+	    List<Customer> adicionados = (List<Customer>) CollectionUtils.subtract(version2, version1);
+    	Collection<Customer> removidos = CollectionUtils.subtract(version1, version2);
+	    
+//    	System.out.println("Adicionados");
+//	    adicionados.forEach(i -> System.out.println(i.getName()));
+//	    
+//	    System.out.println("Removidos");
+//	    removidos.forEach(i -> System.out.println(i.getName()));
+	    
+	    for (Iterator iterator = adicionados.iterator(); iterator.hasNext();) {
+			Customer adicionado = (Customer) iterator.next();
+			for (Iterator iterator2 = removidos.iterator(); iterator2.hasNext();) {
+				Customer removido = (Customer) iterator2.next();
+				if(adicionado.getId().equals(removido.getId())){
+					iterator.remove();
+					iterator2.remove();
+				}
+			}
+		}
+	    
+	    System.out.println("Adicionados (Sem editados)");
+	    adicionados.forEach(i -> System.out.println(i.getName()));
+	    
+	    System.out.println("Removidos (Sem editados)");
+	    removidos.forEach(i -> System.out.println(i.getName()));
+
+//	    assertFalse(result.contains(customer1));
 	}
 }
